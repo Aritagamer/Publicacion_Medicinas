b2005298c02ebec4f279d3ab728fdcbd40ef37df
aritagamer <arita1817@gmail.com>
2022-01-11 05:50:33 -0600

    Otra vez

--- Medicinas/settings.py
+++ Medicinas/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
