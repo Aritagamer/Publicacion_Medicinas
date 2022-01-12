252d3fcf88a57eba273e96b5b98a7a00e1175ca6
aritagamer <arita1817@gmail.com>
2022-01-12 10:47:15 -0600

    Formulario responsive

--- Registro/forms.py
+++ Registro/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from Inventario.models import Medicinas
 
 class Dar_medicamento(forms.Form):
-    Medicamento = forms.CharField(label="Medicamento",required=True, widget=forms.Select(attrs={'class' : 'form-select'}))
-    Dosis  = forms.FloatField(min_value=0, label="Dosis",localize=False,widget=forms.NumberInput(attrs={'class':'form-control'}))
+    Medicamento = forms.CharField(label="Medicamento",required=True, widget=forms.Select(attrs={'class' : 'form-select', "style":"width: 50vw;"}))
+    Dosis  = forms.FloatField(min_value=0, label="Dosis",localize=False,widget=forms.NumberInput(attrs={'class':'form-control', "style":"width: 10vw;"}))
 
     error_css_class = 'alert alert-danger'
     #required_css_class = 'alert alert-warning'
