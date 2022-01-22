38286d2048733990807bed53321af64537aba76a
aritagamer <arita1817@gmail.com>
2022-01-21 21:47:43 -0600

    Horario Ugly

--- Horario/urls.py
+++ Horario/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from Horario import views
 
 urlpatterns = [
-    re_path(r'^(?:\/(?P<dia>[0-7]))?\/$',views.Tabla_Horario,name='vista_Horario'),
+    re_path('/',views.Tabla_Horario,name='vista_Horario'),
     path('/new/',views.New_Horario),
     path('/update/',views.Update_Horario),
     path('/delete/',views.Delete_Horario),
