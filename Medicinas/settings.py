f0b230866ab955b971e348b3303cfe206e5d0449
aritagamer <arita1817@gmail.com>
2022-01-23 20:44:58 -0600

    Multiples medicamentos en horario

--- Medicinas/settings.py
+++ Medicinas/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJ_SECURITY_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 """SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True"""
 
